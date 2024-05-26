67e5952acb5c109d81e1f99a8b7c6c85dc7bcd63
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-05-26 16:19:00 -0500

    ENTITY_RENDER event, allowing to cancel render

--- common/src/main/java/com/thekillerbunny/goofyplugin/ducks/EventsAPIAccess.java
+++ common/src/main/java/com/thekillerbunny/goofyplugin/ducks/EventsAPIAccess.java
@@ -4,4 +4,5 @@ import org.figuramc.figura.lua.api.event.LuaEvent;
 
 public interface EventsAPIAccess {
     public LuaEvent GoofyPlugin$getErrorEvent();
+    public LuaEvent GoofyPlugin$getEntityRenderEvent();
 }
