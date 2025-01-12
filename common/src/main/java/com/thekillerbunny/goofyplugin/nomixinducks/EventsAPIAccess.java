4a5d925dfccaa7dd730e15a5c2f167c990dc508b
PoolloverNathan <nathan.kulzer@protonmail.com>
2025-01-12 02:21:01 -0500

    feat: PRE_RENDER event

--- common/src/main/java/com/thekillerbunny/goofyplugin/nomixinducks/EventsAPIAccess.java
+++ common/src/main/java/com/thekillerbunny/goofyplugin/nomixinducks/EventsAPIAccess.java
@@ -5,5 +5,7 @@ import org.figuramc.figura.lua.api.event.LuaEvent;
 public interface EventsAPIAccess {
     public LuaEvent GoofyPlugin$getErrorEvent();
     public LuaEvent GoofyPlugin$getEntityRenderEvent();
+
+    LuaEvent GoofyPlugin$getPreRenderEvent();
 }
 
