31271761a2ab2deab3184b94c2b13069fb855e98
TheBunnyMan123 <bunny@tkbunny.net>
2026-01-02 22:56:36 -0600

    fix pre-frame-render

--- common/src/main/java/com/thekillerbunny/goofyplugin/mixin/GameRendererMixin.java
+++ common/src/main/java/com/thekillerbunny/goofyplugin/mixin/GameRendererMixin.java
@@ -1,7 +1,6 @@
 package com.thekillerbunny.goofyplugin.mixin;
 
 import com.thekillerbunny.goofyplugin.nomixducks.EventsAPIAccess;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.renderer.GameRenderer;
 import org.figuramc.figura.avatar.AvatarManager;
 import org.luaj.vm2.*;
@@ -13,10 +12,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Inject(at = @At("HEAD"), method = "render", cancellable = true)
-    void render(DeltaTracker tracker, boolean tick, CallbackInfo ci) {
+    void render(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
         for (var avatar: AvatarManager.getLoadedAvatars()) {
             //noinspection DataFlowIssue: the `return null` is never reachable because of the error() call, but Java can't encode never types
-            if (avatar.luaRuntime != null && (Object) avatar.run(((EventsAPIAccess) avatar.luaRuntime.events).GoofyPlugin$getPreFrameRenderEvent(), avatar.worldRender, tracker.getGameTimeDeltaTicks(), null, tick) instanceof Varargs v && v.toboolean(1) && avatar.isHost) ci.cancel();
+            if (avatar.luaRuntime != null && (Object) avatar.run(((EventsAPIAccess) avatar.luaRuntime.events).GoofyPlugin$getPreFrameRenderEvent(), avatar.worldRender, tickDelta, null, tick) instanceof Varargs v && v.toboolean(1) && avatar.isHost) ci.cancel();
         }
     }
 }
